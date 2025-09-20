3daf5b8dc7b8bdf4815d9ded89c29ff6d22da8a9
kovyazinai <kovyazinai@cash-u.com>
2025-09-20 14:17:41 +0300

    add: unit tests for document creating and versionizing

--- documents/tests/unit/document_service.py
+++ documents/tests/unit/document_service.py
@@ -4,31 +4,110 @@ from documents.src.domain.schemas.document import DocumentIn
 from documents.tests.fixtures.factories import create_document_in_data
 
 
-# TODO rewrite to make it pure unit test
-async def test_document_service_create(fake_documents_service):
-    """ Test creating a document through documents service. """
+async def test_document_creation(fake_documents_service):
+    """
+    Test creating single document.
+
+    Checking that all manually set attributes are correct.
+    """
+
+    name = "Test Document",
+    note = "Test note",
+    company_id = str(uuid.uuid4()),
+    project_id = str(uuid.uuid4()),
+    section_id = str(uuid.uuid4()),
+    responsible_id = str(uuid.uuid4()),
+
+    data = create_document_in_data(
+        name=name,
+        note=note,
+        company_id=company_id,
+        project_id=project_id,
+        section_id=section_id,
+        responsible_id=responsible_id
+    )
+
+    document = await fake_documents_service.create(
+        DocumentIn(**data)
+    )
+
+    # check attrs that was set manually
+    assert document.name == name
+    assert document.note == note
+    assert document.company_id == company_id
+    assert document.project_id == project_id
+    assert document.section_id == section_id
+    assert document.responsible_id == responsible_id
+
+    # check generated values
+    assert document.md5 is not None
+    assert document.version == 1
+    assert document.variation == 0
+
+
+async def test_document_versions(fake_documents_service):
+    """
+    Test documents versionizing.
+
+    Creating multiple documents of the same section without raise_variation flag.
+    """
 
     section_id = uuid.uuid4()
     first_variation_data = create_document_in_data(
         name="Document of a specific section",
-        note="First variation",
+        note="First version, zero variation",
         section_id=section_id
     )
     second_variation_data = create_document_in_data(
         name="Document of a specific section",
-        note="Second variation",
+        note="Second version, zero variation",
         section_id=section_id
     )
+
     first_document = await fake_documents_service.create(
-        DocumentIn(**first_variation_data)
+        DocumentIn(**first_variation_data),
+        raise_variation=False,
     )
     second_document = await fake_documents_service.create(
-        DocumentIn(**second_variation_data)
+        DocumentIn(**second_variation_data),
+        raise_variation=False,
     )
 
-    assert first_document.md5 is not None
-    assert first_document.version == 0
-    assert first_document.variation == 1
-    assert second_document.md5 is not None
-    assert second_document.version == 1
-    assert second_document.variation == 2
+    assert first_document.version == 1
+    assert first_document.variation == 0
+    assert second_document.version == 2
+    assert second_document.variation == 0
+
+
+async def test_document_variations(fake_documents_service):
+    """
+    Test documents variations.
+
+    Creating multiple documents of the same section with raise_variation flag.
+    """
+
+    section_id = uuid.uuid4()
+    first_variation_data = create_document_in_data(
+        name="Document of a specific section",
+        note="First variation, zero variation",
+        section_id=section_id
+    )
+    second_variation_data = create_document_in_data(
+        name="Document of a specific section",
+        note="Second variation, first variation",
+        section_id=section_id
+    )
+
+    first_document = await fake_documents_service.create(
+        DocumentIn(**first_variation_data),
+        raise_variation=True,
+    )
+    second_document = await fake_documents_service.create(
+        DocumentIn(**second_variation_data),
+        raise_variation=True,
+    )
+
+    assert first_document.version == 1
+    assert first_document.variation == 0
+    assert second_document.version == 2
+    assert second_document.variation == 1
