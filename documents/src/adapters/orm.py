6f7b3d0911e251aa7b3bbe00968a886c0f34f9ed
kovyazinai <kovyazinai@cash-u.com>
2025-09-20 14:16:39 +0300

    add: comments for OrmDocument fields

--- documents/src/adapters/orm.py
+++ documents/src/adapters/orm.py
@@ -16,8 +16,8 @@ class OrmDocument(Base):
     updated_at = sa.Column(sa.DateTime, onupdate=sa.func.now())
     status = sa.Column(sa.Enum(DocumentStatuses), default=DocumentStatuses.NEW, nullable=False)
     name = sa.Column(sa.String, nullable=False)
-    version = sa.Column(sa.Integer, nullable=False)
-    variation = sa.Column(sa.Integer, nullable=False)
+    version = sa.Column(sa.Integer, nullable=False, comment="Used on expertise")
+    variation = sa.Column(sa.Integer, nullable=False, comment="Continuous numbering for every document")
     md5 = sa.Column(sa.String(32), nullable=False)
     note = sa.Column(sa.String)
     # TODO: doc_path at S3 Minio
