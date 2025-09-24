923b551939137053643d3a2c1474afc043283d6d
kovyazinai <kovyazinai@cash-u.com>
2025-09-24 20:41:10 +0300

    bugfix: delete unused abstract method 'upload' from AbstractDocumentService

--- documents/src/service/documents_service.py
+++ documents/src/service/documents_service.py
@@ -24,14 +24,6 @@ class AbstractDocumentService(abc.ABC):
     ) -> DocumentOut:
         ...
 
-    @abc.abstractmethod
-    async def upload(
-            self,
-            file_path: str,
-            file_content: bytes
-    ):
-        ...
-
 
 class DocumentService(AbstractDocumentService):
 
