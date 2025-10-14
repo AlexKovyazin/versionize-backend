d86ed0b46fe810edea5feeaf0d271ef66b39c0e1
kovyazinai <kovyazinai@cash-u.com>
2025-10-14 20:30:00 +0300

    add (identity): validated field default value

--- identity/src/adapters/orm.py
+++ identity/src/adapters/orm.py
@@ -45,7 +45,7 @@ class OrmUser(BaseMixin, Base):
     position: Mapped[str | None]
     project_role: Mapped[UserProjectRole | None]
     last_login: Mapped[datetime | None]
-    validated: Mapped[bool] = mapped_column(default=False)
+    validated: Mapped[bool] = mapped_column(server_default="false")
     company_id: Mapped[str] = mapped_column(
         sa.UUID, sa.ForeignKey("companies.id"), index=True
     )
