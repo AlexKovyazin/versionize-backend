7cee976f965e164a013ebe74b3c6a027c13bec09
kovyazinai <kovyazinai@cash-u.com>
2025-10-16 15:22:45 +0300

    refactor (documents): OrmDocument to mapped style

--- documents/src/adapters/orm.py
+++ documents/src/adapters/orm.py
@@ -1,7 +1,8 @@
 import uuid
+from datetime import datetime
 
 import sqlalchemy as sa
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
 from documents.src.enums import DocumentStatuses
 
@@ -11,21 +12,21 @@ Base = declarative_base()
 class OrmDocument(Base):
     __tablename__ = "documents"
 
-    id = sa.Column(sa.UUID, primary_key=True, default=uuid.uuid4, index=True)
-    created_at = sa.Column(sa.DateTime, server_default=sa.func.now(), nullable=False, index=True)
-    updated_at = sa.Column(sa.DateTime, onupdate=sa.func.now())
-    status = sa.Column(sa.Enum(DocumentStatuses), default=DocumentStatuses.NEW, nullable=False)
-    name = sa.Column(sa.String, nullable=False)
-    version = sa.Column(sa.Integer, nullable=False, comment="Used on expertise")
-    variation = sa.Column(sa.Integer, nullable=False, comment="Continuous numbering for every document")
-    md5 = sa.Column(sa.String(32), nullable=False, unique=True)
-    note = sa.Column(sa.String)
-    doc_path = sa.Column(sa.String)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4, index=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=sa.func.now(), index=True)
+    updated_at: Mapped[datetime | None] = mapped_column(server_default=sa.func.now())
+    status: Mapped[DocumentStatuses] = mapped_column(default=DocumentStatuses.NEW)
+    name: Mapped[str]
+    version: Mapped[int] = mapped_column(comment="Used on expertise")
+    variation: Mapped[int] = mapped_column(comment="Continuous numbering for every document")
+    md5: Mapped[str] = mapped_column(sa.String(32), unique=True)
+    note: Mapped[str | None]
+    doc_path: Mapped[str | None]
 
-    company_id = sa.Column(sa.UUID, nullable=False, index=True)
-    project_id = sa.Column(sa.UUID, nullable=False, index=True)
-    section_id = sa.Column(sa.UUID, nullable=False, index=True)
-    responsible_id = sa.Column(sa.UUID, nullable=False, index=True)
+    company_id: Mapped[uuid.UUID] = mapped_column(index=True)
+    project_id: Mapped[uuid.UUID] = mapped_column(index=True)
+    section_id: Mapped[uuid.UUID] = mapped_column(index=True)
+    responsible_id: Mapped[uuid.UUID] = mapped_column(index=True)
 
     def to_dict(self, exclude: list = None):
         """Convert model instance to dictionary."""
